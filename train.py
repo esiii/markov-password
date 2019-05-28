6f35b1eaf6f3ec5535e68eb4fa81d061a67d065b
esiii <915663689@qq.com>
2019-05-28 10:59:03 +0800

    训练数据

---
+++ train.py
@@ -0,0 +1,58 @@
+import pickle
+import math
+
+stats = {}
+max_ngrams = 1
+
+
+def ngram(line, n):
+	output = []
+	for i, char in enumerate(line):
+		if i - n < 0:
+			buff = ''
+			for j in range(abs(i - n)):
+				buff += 'Ω'
+			buff += str(line[0:i])
+			output.append((buff, char))
+		else:
+			output.append((line[i - n:i], char))
+	#print(output)
+	return output
+
+def DelLastChar(str):
+	str_list=list(str)
+	str_list.pop()
+	return "".join(str_list)
+
+
+with open('/Users/esicao/Desktop/markov/markov-passwords-master/data/yuanshi2','rb') as file:
+#with open('/Users/esicao/Desktop/markov/markov-passwords-master/data/tt.txt', 'rb') as file:
+	for line in file:
+		line = line.decode("latin-1")
+		line = line.replace('\n','å')
+		#line = DelLastChar(line)
+
+
+		for i in range(max_ngrams):
+			for gram in ngram(line, i + 1):
+				prev = gram[0] # previous characters, ngram
+				nxt = gram[1] # next character
+				if not prev in stats:
+					stats[prev] = {}
+				if not nxt in stats[prev]:
+					stats[prev][nxt] = 0
+				
+				stats[prev][nxt] += 1
+
+for ngram in stats:
+	total = 0
+
+	for key, value in stats[ngram].items():
+		total += value
+
+	for key, value in stats[ngram].items():
+		stats[ngram][key] = value / float(total)
+
+with open('/Users/esicao/Desktop/markov/markov-passwords-master/data/yuanshi-{}-gram.pickle'.format(max_ngrams), 'wb') as file:
+	pickle.dump(stats, file)
+
